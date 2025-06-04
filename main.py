8390c671a725ce0a28b02d2df61ce154e39fcb8d
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-04 18:45:54 +0100

    LIAR

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import random #importing module
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
 
-playing = True #initialise
-number = str(random.randint(10,20)) #random in-built function
-
-print("I will generate a number from 10 to 20, and you have to guess the number one digit at a time.")
-print("The game ends when you get 1 hero!")
-#iterate loop till the condition is true
-while playing:
-    guess = input("Give me your best guess! \n")
-    if number == guess:
-        print("You win the game")
-        print("The number was",number)
-        break
-    else:
-        print("Your guess isn't quite right, try again. \n")
+print(math.ceil(45.490))
+print(math.floor(45.890))
+print(math.factorial(5))
